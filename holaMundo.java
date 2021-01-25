a9f0c5db3f545c9cbdbb55ae1066bd38b4e8e541
Peyo83 <42756904+Peyo83@users.noreply.github.com>
2021-01-25 18:00:50 +0100

    Ejemplo Amazon clases creadas

---
+++ holaMundo.java
@@ -0,0 +1,10 @@
+
+/**
+ * holaMundo
+ */
+public class holaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ newfor/src/newfor/newfor.java
@@ -0,0 +1,20 @@
+package newfor;
+
+import java.util.ArrayList;
+public class NewFor {
+  public static void main(String[] args) {
+    
+    ArrayList<String> lista = new ArrayList<String>();
+    
+    lista.add("hola");
+    lista.add("que");
+    lista.add("tal");
+    lista.add("estas");
+    lista.add("hoy");
+    
+    for (int i=0;i<lista.size();i++) {
+      
+      System.out.println(lista.get(i));
+    }
+  }
+}
